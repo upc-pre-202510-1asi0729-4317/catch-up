fdbfe85be05399a467382fe87cb4e702dd4d6dc9
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-04-26 15:34:22 -0500

    chore: add environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  newsProviderApiBaseUrl: 'https://newsapi.org/v2',
+  newsProviderNewsEndpointPath: '/top-headlines',
+  newsProviderSourcesEndpointPath: '/top-headlines/sources',
+  newsProviderApiKey: 'cb8453a97bc8484291438f15d21dbc06',
+  logoProviderApiBaseUrl: 'https://logo.clearbit.com',
+}
